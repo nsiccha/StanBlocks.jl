bf7e1d06155dff543dcf3273299148fd251b8cd1
Nikolas Siccha <nikolassiccha@gmail.com>
2024-12-04 11:56:42 +0100

    Built site for gh-pages

--- ext/LogDensityProblemsExt.jl
+++
@@ -1,6 +0,0 @@
-module LogDensityProblemsExt
-import StanBlocks, LogDensityProblems
-LogDensityProblems.capabilities(::Type{<:StanBlocks.VectorPosterior}) = LogDensityProblems.LogDensityOrder{0}()
-LogDensityProblems.dimension(p::StanBlocks.VectorPosterior) = StanBlocks.dimension(p)
-LogDensityProblems.logdensity(p::StanBlocks.VectorPosterior, x) = p(x)
-end
--- src/wrapper.jl
+++
@@ -1,6 +0,0 @@
-struct VectorPosterior{F,N}
-    f::F
-    n::N
-end
-@inline (p::VectorPosterior)(x) = p.f(x)
-@inline dimension(p::VectorPosterior) = p.n
