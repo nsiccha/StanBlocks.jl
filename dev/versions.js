4e3dd335a55d8cb9f0bfda24acf8c3f4f76025d9
Documenter.jl <documenter@juliadocs.github.io>
2026-07-22 12:49:39 +0000

    build based on 35dfcab

---
+++ dev/versions.js
@@ -0,0 +1,3 @@
+// Stub for local dev. DocumenterVitepress overwrites this on deploy
+// with the real version list used by the navbar version-switcher.
+window.DOC_VERSIONS = window.DOC_VERSIONS || [];
